3a9fcc606ce759b616cc983cb0b19acf0e58fbbc
unknown <r.belmonte8@gmail.com>
2020-06-07 13:39:37 +0200

    import BandsService into app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Routes
+
+
+//Services
+import { BandsService } from './services/bands.service';
+
+// Components
+
 import { AppComponent } from './app.component';
 import { AllBandsComponent } from './components/all-bands/all-bands.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
@@ -14,7 +22,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [BandsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
